0df9aeb507d8751d155a879a88ad8caea3cee10d
Fernando Puri <luiz.gomez@rebase.com.br>
2024-04-04 18:16:39 -0300

    rest api consumes message sent by graphql api and creates policy

--- app/workers/policy_receiver_worker.rb
+++ app/workers/policy_receiver_worker.rb
@@ -6,7 +6,16 @@ class PolicyReceiverWorker
   from_queue 'create_policy'
 
   def work(msg)
-    PostPolicy.call
+    msg = JSON.parse(msg)
+    segurado = Segurado.create(**msg["segurado"])
+    veiculo = Veiculo.create(**msg["veiculo"])
+    Policy.create(
+      data_emissao: msg["data_emissao"],
+      data_fim_cobertura: msg["data_fim_cobertura"],
+      segurado_id: segurado.id,
+      veiculo_id: veiculo.id
+    )
+
     puts "#{msg}"
     puts "FOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOIIIIIIIIII"
     ack!
