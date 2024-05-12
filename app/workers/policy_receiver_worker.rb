ff2285a7fe1e69273429b02624d77bab0bd597fe
Fernando Puri <luiz.gomez@rebase.com.br>
2024-05-12 15:31:05 -0300

    Create charge model, and add status and charge_id in Policy

--- app/workers/policy_receiver_worker.rb
+++ app/workers/policy_receiver_worker.rb
@@ -9,11 +9,14 @@ class PolicyReceiverWorker
     msg = JSON.parse(msg)
     segurado = Segurado.create(**msg["segurado"])
     veiculo = Veiculo.create(**msg["veiculo"])
+    charge = Charge.create(**msg["charge"])
+
     Policy.create(
       data_emissao: msg["data_emissao"],
       data_fim_cobertura: msg["data_fim_cobertura"],
       segurado_id: segurado.id,
-      veiculo_id: veiculo.id
+      veiculo_id: veiculo.id,
+      charge_id: charge.id
     )
 
     puts "#{msg}"
